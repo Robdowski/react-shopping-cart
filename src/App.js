3abfba49579fb6da76a3ebb5a02cfa586003741d
Robert <rc3331@gmail.com>
2019-10-07 14:21:25 -0500

    committing mvp including cartcontext and productcontext

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import data from "./data";
 
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -19,17 +20,15 @@ function App() {
 
   return (
     <div className="App">
-      <ProductContext.Provider value={{products, addItem}}>
-        <Navigation cart={cart} />
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart }}>
+          <Navigation />
 
-        {/* Routes */}
-        <Route
-          exact
-          path="/"
-          component={Products}
-        />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
 
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
